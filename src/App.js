7480f1b9b430f3ace617ce618e448dd3edcfd2fc
jatinkashyap <jatinkashyap9@gmail.com>
2018-10-13 09:34:46 -0400

    npm update

--- src/App.js
+++ src/App.js
@@ -6,13 +6,28 @@ import Backdrop from "./components/Backdrop/Backdrop";
 import List from "./components/List/List";
 
 class App extends Component {
+
+  state = {
+    isModalOpen : false
+  }
+
+  openModalHandler = () => {
+    console.log("Hii");
+    this.setState({isModalOpen : true});
+  }
+
+  closeModalHandler = () => {
+    console.log("close called");
+    this.setState({isModalOpen : false});
+  }
+
   render() {
     return (
       <div className="App">
         <h1>React Animations</h1>
-        <Modal />
-        <Backdrop />
-        <button className="Button">Open Modal</button>
+        <Modal show={this.state.isModalOpen} closed={this.props.closeModalHandler}/>
+        <Backdrop show={this.state.isModalOpen} /> 
+        <button className="Button" onClick={this.openModalHandler}>Open Modal</button>
         <h3>Animating Lists</h3>
         <List />
       </div>
